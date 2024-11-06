57e0d294347e9678f8c8a536b94229415dce2281
Omershat <shatzo772@gmail.com>
2024-11-06 20:08:29 +0200

    Add files via upload

--- Queue.cpp
+++ Queue.cpp
@@ -4,16 +4,16 @@
 
 
 
-
+// iterate the array and set each element to zero
 void cleanQueue(Queue* q)
 {
 	for (int i = 0; i < sizeof(q->values);i++) 
 	{
-		q->values[i] = 0;
+		q->values[i] = 0;// here we reset each value to zero
 	}
 
-	q->maxElementsNum = 0;
-	q->elementNum = 0;
+	q->maxElementsNum = 0; // reset the max cap in the queue
+	q->elementNum = 0; // reset the number of element
 
 
 }
@@ -22,10 +22,12 @@ void cleanQueue(Queue* q)
 void initQueue(Queue* q, unsigned int size)
 {
 	q->values = new unsigned int[size];
+	
+	// each element was set to zero
 
-	for (size_t i = 0; i < size; i++) 
+	for (size_t i = 0; i < size; i++) // i used this after a serch in google to store non negative values
 	{
-		q->values[i] = 0;
+		q->values[i] = 0; // reeset to zero
 
 	}
 	q->maxElementsNum = size;
@@ -35,8 +37,15 @@ void initQueue(Queue* q, unsigned int size)
 
 int dequeue(Queue* q)
 {
+	// decreasing the count
 	q->elementNum -= 1;
+
+	// getting the values after the decresment 
+
 	unsigned int retrunValue = q->values[q->elementNum];
+
+	// clearing the slot
+
 	q->values[q->elementNum] = 0;
 
 	return retrunValue;
@@ -45,19 +54,24 @@ int dequeue(Queue* q)
 
 void enqueue(Queue* q, unsigned int newValue)
 {
-	
+	// cheaking if the queue if full
 	if (q->elementNum >= q->maxElementsNum)
 	{
+		// increasing one
 		q->maxElementsNum += 1;
 
+		// assigned new memory for the expended array 
 		unsigned int* tempPointer = new unsigned int[q->maxElementsNum];
 
+		// getting the old values
+		
 		for (int i = 0; i < q->maxElementsNum; i++)
 		{
 			tempPointer[i] = q->values[i];
 		}
 
-		delete[] q->values;
+		// Deallocate memory of the old array
+		delete[] q->values; // i used this from google to understand how to delocate the memory
 		q->values = tempPointer;
 	}
 
@@ -69,11 +83,13 @@ void enqueue(Queue* q, unsigned int newValue)
 
 bool isEmpty(Queue* s)
 {
+	// return if the queue is empty 
 	return s->elementNum == 0;
 }
 
 bool isFull(Queue* s) 
 {
+	// return if the queue is full
 	 return s->elementNum == s->maxElementsNum;
 }
 
