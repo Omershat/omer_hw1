6232372871b63a9c36f6af52ee8b4ff73f8cc43f
Omershat <shatzo772@gmail.com>
2024-11-06 19:39:34 +0200

    Add files via upload

--- Queue.cpp
+++ Queue.cpp
@@ -9,11 +9,11 @@ void cleanQueue(Queue* q)
 {
 	for (int i = 0; i < sizeof(q->values);i++) 
 	{
-		q->values[i] = NULL;
+		q->values[i] = 0;
 	}
 
-	q->maxElementsNum = NULL;
-	q->elementNum = NULL;
+	q->maxElementsNum = 0;
+	q->elementNum = 0;
 
 
 }
@@ -25,7 +25,7 @@ void initQueue(Queue* q, unsigned int size)
 
 	for (size_t i = 0; i < size; i++) 
 	{
-		q->values[i] = NULL;
+		q->values[i] = 0;
 
 	}
 	q->maxElementsNum = size;
@@ -37,7 +37,7 @@ int dequeue(Queue* q)
 {
 	q->elementNum -= 1;
 	unsigned int retrunValue = q->values[q->elementNum];
-	q->values[q->elementNum] = NULL;
+	q->values[q->elementNum] = 0;
 
 	return retrunValue;
 }
@@ -66,3 +66,16 @@ void enqueue(Queue* q, unsigned int newValue)
 
 	q->elementNum += 1;
 }
+
+bool isEmpty(Queue* s)
+{
+	return s->elementNum == 0;
+}
+
+bool isFull(Queue* s) 
+{
+	 return s->elementNum == s->maxElementsNum;
+}
+
+
+
