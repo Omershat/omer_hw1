3f8ea6a9d6e1f19a113a0c756fcf9e2f83e7c4a3
Omershat <shatzo772@gmail.com>
2024-11-08 17:23:52 +0200

    Add files via upload

--- linkedList.cpp
+++ linkedList.cpp
@@ -6,7 +6,19 @@ void remove(linkedList* firstNote)
 	linkedList* oldFirst = firstNote->next;
 
 	firstNote->next = oldFirst->next;
-	oldFirst->next = NULL;
-	oldFirst->value = NULL;
+	oldFirst->next = 0;
+	oldFirst->value = 0;
 	delete oldFirst;
 }
+
+void add(linkedList* firstNote, unsigned int value)
+{
+	linkedList* oldFirst = firstNote->next;
+
+	linkedList* newFirst = new linkedList;
+	newFirst->value = value;
+	newFirst->next = oldFirst;
+
+	firstNote->next = newFirst;
+}
+
