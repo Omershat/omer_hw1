33a9d71c5a88482228d22a982e98d18fc1b18127
Omershat <shatzo772@gmail.com>
2024-11-08 17:44:07 +0200

    Add files via upload

--- stack.cpp
+++ stack.cpp
@@ -6,3 +6,20 @@ void initStack(Stack* s)
 	s->value = 0;
 	s->before = 0;
 }
+
+void cleanStack(Stack* s)
+{
+	stack* lastNote = s;
+	stack* tempPoninter;
+
+	while (lastNote)
+	{
+		tempPoninter = lastNote->before;
+
+		lastNote->value = NULL;
+		lastNote->before = NULL;
+		delete lastNote;
+
+		lastNote = tempPoninter;
+	}
+}
